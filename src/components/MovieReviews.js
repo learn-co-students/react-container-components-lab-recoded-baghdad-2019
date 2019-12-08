84cd252750c6b39f5519923fa7cf1058151453f5
dhuha <duha_9.com>
2019-12-08 18:47:14 +0300

    try1

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,10 @@
-// Code MovieReviews Here
+import React from 'react'
+
+
+let MovieReviews = ({ reviwes }) =>
+
+      <ul className="review-list">{reviwes ? reviwes.map(review =>{
+             return <li className='review'>{review.display_title}</li>}) : null}
+      </ul>
+
+export default MovieReviews
